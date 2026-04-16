13464e9d8b58d8c00cf255149513f1f38808155e
Yuna-Miyazaki <miyazaki.yuna.nf7@naist.ac.jp>
2026-04-16 20:06:28 +0900

    change

---
+++ RemoteApp/RemoteApp/RemoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteAppApp.swift
+//  RemoteApp
+//
+//  Created by miyazaki yuna on 2026/04/16.
+//
+
+import SwiftUI
+
+@main
+struct RemoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
